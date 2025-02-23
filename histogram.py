139fcd45ef1fbc7fa753a983f0db05237ce8d8db
michprk <michaelpark1102@gmail.com>
2025-02-23 03:26:31 -0500

    Imported Normalized Histogram Algorithm from finding_tori (prototype)

--- histogram.py
+++ histogram.py
@@ -2,7 +2,10 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import os 
+import math
 from scipy.signal import medfilt 
+from collections import Counter 
+
 
 input_dir = '/home/sangheon/Desktop/Pansori_2025_ISMIR/PansoriData/F0'
 output_dir = '/home/sangheon/Desktop/Pansori_2025_ISMIR/PansoriData/Histogram'
@@ -11,7 +14,7 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 def f0_to_midi(f0):
-    return 69 + 12 * np.log2(f0 / 440)
+    return 69 + 12 * math.log2(f0 / 440)
 
 def midi_to_note(midi_number):
     note_names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
@@ -24,6 +27,29 @@ def midi_to_note(midi_number):
 def apply_median_filter(f0_series, kernel_size=5):
 
     return medfilt(f0_series, kernel_size=kernel_size)
+
+def get_norm_pitch_histogram(midi_contour, compensate_tuning = True):
+
+    edge = [i + (-12.5) for i in range(26)]
+    max_appearance = 0
+    comp = 0
+    final_common_midi = 0
+
+    if compensate_tuning:
+        for compensate in (0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9):
+            intmidi = [round(midi + compensate) for midi in midi_contour]
+            common_pitch, num_appearance = Counter(intmidi).most_common()[0]
+            if max_appearance <= num_appearance:
+                max_appearance = num_appearance
+                comp = compensate
+                final_common_midi = common_pitch
+            else:
+                intmidi = [round(midi) for midi in midi_contour]
+                common_pitch, num_appearance = Counter(intmidi).most_common()[0]
+                final_common_midi = common_pitch
+                final_common_midi = final_common_midi - comp 
+                
+    return final_common_midi
 
 def main():
     for file_name in os.listdir(input_dir):

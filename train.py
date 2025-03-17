330327e46a5a486c77901776830e15f6f16e39dc
jinin <jcastle5113@gmail.com>
2025-03-18 00:06:47 +0900

    Fix valid segments

--- train.py
+++ train.py
@@ -7,7 +7,8 @@ import datetime
 from pathlib import Path
 
 import numpy as np
-from sklearn.model_selection import KFold
+from tqdm import tqdm
+from sklearn.model_selection import KFold, LeaveOneOut
 
 import hydra
 import wandb
@@ -32,10 +33,6 @@ def set_seed(seed=42):
 
 @hydra.main(config_path="configs", config_name="train")
 def main(cfg):
-    run_name = f'{cfg.models.cls}'
-    wandb.init(project='Pansori_Mode_Detection', name=run_name)
-    wandb.config.update(OmegaConf.to_container(cfg))
-
     set_seed(cfg.train.random_seed)
 
     audio_dir = cfg.data.audio_dir
@@ -55,29 +52,38 @@ def main(cfg):
     dataset = dataset_class(audio_dir, label_json, **dataset_params)
     print(f"Length of dataset: {len(dataset)}")
 
+    criterion = nn.CrossEntropyLoss()
+    selection = KFold(**cfg.kfold) if cfg.train.selection=="KFold" else LeaveOneOut(**cfg.loo)
+
     model_name = cfg.models.cls
     model_params = OmegaConf.to_container(cfg.models.cfg)
     model_class = getattr(models, model_name)
-    model = model_class(**model_params)
-    model = nn.DataParallel(model) if torch.cuda.device_count() > 1 else model.to(DEV)
-    num_model_parameters = sum(p.numel() for p in model.parameters())
-    wandb.summary['Num model parameters'] = num_model_parameters
-    print(f"Sum of Model Parameters: {num_model_parameters}")
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = Adam(model.parameters(), lr=cfg.train.lr)
-    kfold = KFold(**cfg.kfold)
-
-    trainer = Trainer(model=model, 
-                      dataset=dataset, 
-                      optimizer=optimizer,
+    trainer = Trainer(dataset=dataset, 
                       criterion=criterion,
-                      device=DEV, 
-                      kfold=kfold, 
+                      device=DEV,
                       save_dir=save_dir, 
                       best_dir=best_dir,
                       config=cfg)
-    trainer.train()
+
+    fold_best_acc = {}
+    for idx, (train_idx, test_idx) in enumerate(selection.split(range(len(trainer.dataset)))):
+        if wandb.run is not None: wandb.finish()
+        run_name = f'{cfg.models.cls}_Fold{idx+1}_{datetime.datetime.now().strftime("%m%d_%H%M")}'
+        wandb.init(project='Pansori_Mode_Detection', name=run_name, reinit=True)
+        wandb.config.update(OmegaConf.to_container(cfg))
+
+        model = model_class(**model_params)
+        model = nn.DataParallel(model) if torch.cuda.device_count() > 1 else model.to(DEV)
+        num_model_parameters = sum(p.numel() for p in model_class(**model_params).parameters())
+        wandb.summary['Num model parameters'] = num_model_parameters
+
+        optimizer = Adam(model.parameters(), lr=cfg.train.lr)
+
+        fold_best_acc[idx] = trainer.train_split(idx, train_idx, test_idx, model, optimizer)
+
+    print(f"KFold Average Accuracy: {sum(fold_best_acc.values())}")
+    if wandb.run is not None: wandb.finish()
 
     
 if __name__ == "__main__":

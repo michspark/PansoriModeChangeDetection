93e8d0c26741f7253ba7969257966e65ea691b37
SeongUkJu <jcastle5113@gmail.com>
2025-06-02 21:16:34 +0900

    Refactor 2D Model

---
+++ models/Conv2DGRU.py
@@ -0,0 +1,45 @@
+from models.modules import Conv2DBlock
+from models.model_utils import calc_conv_param, filter_by_confidence
+
+import torch.nn as nn
+
+class Conv2DGRU(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.params = calc_conv_param(config)
+
+        self.kernel_size = config.kernel_size
+        self.dilation = config.dilation
+        self.num_classes = config.num_classes
+        self.enc = self.build()
+
+        self.hidden_dim = config.hidden_dim
+        self.num_gru = config.num_gru
+        self.dropout = config.dropout
+
+        if config.pool_size: out_freq = config.num_bins // (2 ** config.num_layers)
+        else: out_freq = config.num_bins
+        self.gru = nn.GRU(input_size=self.params[-1]['output_channel']*out_freq, hidden_size=self.hidden_dim, num_layers=self.num_gru, batch_first=True, dropout=self.dropout, bidirectional=True)
+        self.fc = nn.Linear(self.hidden_dim*2, self.num_classes)
+
+    def build(self):
+        enc = nn.Sequential()
+        for idx, param in enumerate(self.params):
+            enc.add_module(f'conv_{idx}', Conv2DBlock(param['input_channel'], param['output_channel'], 
+                                                           kernel_size=self.kernel_size, padding='same', dilation=eval(self.dilation)))
+            if self.config.pool_size: enc.add_module(f'pool_{idx}', nn.MaxPool2d(eval(param['max_pool'])))
+        return enc
+
+    def forward(self, x):
+        if x.ndim == 3: x = x.unsqueeze(1)
+
+        x = self.enc(x) # bcft
+        b, _, _, t = x.shape # b,c,f,t
+        x = x.permute(0,3,1,2) # b,t,c,f
+        x = x.reshape(b, t, -1) # b,t,cf
+
+        x, _ = self.gru(x) # b,t,2h
+        x = self.fc(x) # b,t,num_classes
+
+        return x
--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from .modules import *
 from .Conv1DGRU import Conv1DGRU
+from .Conv2DGRU import Conv2DGRU
